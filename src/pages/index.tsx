8612740458040a4182125b217ed2baea17030e50
Ale dev <pimentelaleee@gmail.com>
2023-09-07 17:23:25 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,63 +49,26 @@ export default function Home() {
     }
     setIsLoading(false);
   };
-  const handleLinkAccount = async () => {
-    setIsLoading(true);
-    try {
-      const queryData = {
-        to,
-        subject,
-        message,
-      };
-
-      const response = await fetch('/api/sendemail', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(queryData),
-      });
-
-      if (!response.ok) {
-        throw new Error('Error en la solicitud');
-      }
-
-      const data = await response.json();
-      console.log('Datos del webhook:', data);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-    setIsLoading(false);
-  };
 
   const handleLinkEmail = async () => {
-    const clientId = '8ynk5iozc6zhsjw8stk3z9a5a'
-    const redirectUri = 'https://testing-ventia.vercel.app/'
-
-    const result = await fetch(`https://api.nylas.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer 6iToeQOAkJjePIeFQh2CkGA5F6aXK4`,
-        'Content-Type': 'application/json',
-      },
-      redirect: 'follow'
-    }).then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+    const clientId = '8ynk5iozc6zhsjw8stk3z9a5a';
+    const redirectUri = 'https://testing-ventia.vercel.app/';
+    const responseType = 'code';
+    const authUrl = `https://api.nylas.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`;
+    window.location.href = authUrl;
   }
+
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
     >
-
+      <h1 className='m-4 font-bold'>Bienvenido, haz click y asocia tu Email</h1>
       <Button onClick={handleLinkEmail}>
-        Linkear Cuenta de Email
+        Autentificar Email
       </Button>
       {/* ------------------ */}
-      <Input
+      {/* <Input
         className='my-4'
         type="email"
         placeholder="To"
@@ -125,10 +88,10 @@ export default function Home() {
         value={message}
         onChange={(e) => setMessage(e.target.value)}
       />
-      <Button onClick={handleKnowAccount} disabled={isLoading}>
+      <Button onClick={handleLinkEmail} disabled={isLoading}>
         <ArrowBigDown className="mr-2 h-4 w-4" />
         Enviar Mail
-      </Button>
+      </Button> */}
     </main >
   );
 }

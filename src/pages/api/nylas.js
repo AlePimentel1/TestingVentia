1a8ff5789f21178cd9a84f32197b06696e9cd56e
Ale dev <pimentelaleee@gmail.com>
2023-09-08 14:41:01 -0300

    fix

--- src/pages/api/nylas.js
+++ src/pages/api/nylas.js
@@ -1,6 +1,6 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req, res) {
     if (req.method === "GET" && req.query.challenge) {
         console.log(`Recieved challenge code! - ${req.query.challenge}`);
         console.log(`Now returning challenge code! - ${req.query.challenge}`);
@@ -9,7 +9,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
     if (req.method === "POST") {
         console.log('=======Message Updated Started=======');
-        req.body.deltas.map((delta: any) => console.log(JSON.stringify(delta)));
+        req.body.deltas.map((delta) => console.log(JSON.stringify(delta)));
         console.log('=======Message Updated End=======');
         return res.status(200).end();
     }

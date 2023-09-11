620832e788e893cf4c23dbe1a16e20a09ca514fc
Ale dev <pimentelaleee@gmail.com>
2023-09-11 10:10:02 -0300

    fix

--- src/pages/api/connectaccount.ts
+++ src/pages/api/connectaccount.ts
@@ -15,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         case 'POST': {
             // const authCode = req.query.code;
             if (req.query) {
-                return res.status(200).send({ codigo: `El codigo es: ${JSON.stringify(req.query)} hola hola` })
+                return res.status(200).send({ codigo: `El codigo es: ${JSON.stringify(req.query.code)} hola hola` })
             }
             // return res.status(200).json({ authCode });
             // } else {

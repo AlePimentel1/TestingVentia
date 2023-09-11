9ad362dff26f5a0250c134da294da5638db7e464
Ale dev <pimentelaleee@gmail.com>
2023-09-11 14:48:13 -0300

    fix

--- src/pages/api/reademail.ts
+++ src/pages/api/reademail.ts
@@ -9,30 +9,44 @@ export async function handler(req: NextApiRequest, res: NextApiResponse) {
 }
 
 const read = async (_: NextApiRequest, res: NextApiResponse) => {
-    try {
-        const myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer YAIt2ot9BtuQJF3TbMDCJw9LnPogrf");
-        myHeaders.append("Cookie", "__cf_bm=8VhaZOVXcpc3Yd09lHjaLFdQu_faQ3NgtPZkVVTb2IY-1694453477-0-ASLRfzicN5WMG3vFUAfzaNe0OfCXkrxsUX4K/aUysmrz0zyygFHZLGK0IypsvZTmKW/ccz6OT/1r9AxtFTxFZX0=");
+    // try {
+    //     const myHeaders = new Headers();
+    //     myHeaders.append("Authorization", "Bearer YAIt2ot9BtuQJF3TbMDCJw9LnPogrf");
+    //     myHeaders.append("Cookie", "__cf_bm=8VhaZOVXcpc3Yd09lHjaLFdQu_faQ3NgtPZkVVTb2IY-1694453477-0-ASLRfzicN5WMG3vFUAfzaNe0OfCXkrxsUX4K/aUysmrz0zyygFHZLGK0IypsvZTmKW/ccz6OT/1r9AxtFTxFZX0=");
 
-        const requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow' as RequestRedirect, // Asigna 'follow' como RequestRedirect
-        };
+    //     const requestOptions = {
+    //         method: 'GET',
+    //         headers: myHeaders,
+    //         redirect: 'follow' as RequestRedirect, // Asigna 'follow' como RequestRedirect
+    //     };
 
-        const response = await fetch("https://api.nylas.com/messages?limit=5", requestOptions);
+    //     const response = await fetch("https://api.nylas.com/messages?limit=5", requestOptions);
 
-        if (!response.ok) {
-            throw new Error('Error en la solicitud GET');
-        }
+    //     if (!response.ok) {
+    //         throw new Error('Error en la solicitud GET');
+    //     }
 
-        const result = await response.text();
+    //     const result = await response.text();
 
-        res.status(200).json({ emails: result });
+    //     res.status(200).json({ emails: result });
 
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
+    // } catch (error: any) {
+    //     res.status(500).json({ error: error.message });
+    // }
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer YAIt2ot9BtuQJF3TbMDCJw9LnPogrf");
+    myHeaders.append("Cookie", "__cf_bm=8VhaZOVXcpc3Yd09lHjaLFdQu_faQ3NgtPZkVVTb2IY-1694453477-0-ASLRfzicN5WMG3vFUAfzaNe0OfCXkrxsUX4K/aUysmrz0zyygFHZLGK0IypsvZTmKW/ccz6OT/1r9AxtFTxFZX0=");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow' as RequestRedirect
+    };
+
+    fetch("https://api.nylas.com/messages?limit=5", requestOptions)
+        .then(response => response.text())
+        .then(result => res.status(200).json({ emails: result }))
+        .catch(error => res.status(404).send(error.message));
 }
 
 
